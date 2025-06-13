639dbbbd2c280e7afc31e1ab23541584502482e4
rat <rlaalstlr2001@gmail.com>
2025-06-13 09:21:07 +0900

    refactor: NotionBlockParser key-value mapping 적용

--- service/NotionBlockParser.js
+++ service/NotionBlockParser.js
@@ -1,51 +1,33 @@
 class NotionBlockParser {
+  static typeMap = {
+    paragraph: { prefix: "", get: (b) => b.paragraph?.rich_text },
+    quote: { prefix: "> ", get: (b) => b.quote?.rich_text },
+    heading_1: { prefix: "# ", get: (b) => b.heading_1?.rich_text },
+    heading_2: { prefix: "## ", get: (b) => b.heading_2?.rich_text },
+    heading_3: { prefix: "### ", get: (b) => b.heading_3?.rich_text },
+    bulleted_list_item: {
+      prefix: "- ",
+      get: (b) => b.bulleted_list_item?.rich_text,
+    },
+    numbered_list_item: {
+      prefix: "1. ",
+      get: (b) => b.numbered_list_item?.rich_text,
+    },
+    to_do: { prefix: "[ ] ", get: (b) => b.to_do?.rich_text },
+    toggle: { prefix: "▸ ", get: (b) => b.toggle?.rich_text },
+  };
+
   static blockToText(block) {
-    if (block.paragraph && block.paragraph.rich_text) {
-      return block.paragraph.rich_text.map((t) => t.plain_text).join("");
+    for (const [type, { prefix, get }] of Object.entries(this.typeMap)) {
+      if (block[type] && get(block)) {
+        return (
+          prefix +
+          get(block)
+            .map((t) => t.plain_text)
+            .join("")
+        );
+      }
     }
-
-    if (block.quote && block.quote.rich_text) {
-      return "> " + block.quote.rich_text.map((t) => t.plain_text).join("");
-    }
-
-    if (block.heading_1 && block.heading_1.rich_text) {
-      return "# " + block.heading_1.rich_text.map((t) => t.plain_text).join("");
-    }
-
-    if (block.heading_2 && block.heading_2.rich_text) {
-      return (
-        "## " + block.heading_2.rich_text.map((t) => t.plain_text).join("")
-      );
-    }
-
-    if (block.heading_3 && block.heading_3.rich_text) {
-      return (
-        "### " + block.heading_3.rich_text.map((t) => t.plain_text).join("")
-      );
-    }
-
-    if (block.bulleted_list_item && block.bulleted_list_item.rich_text) {
-      return (
-        "- " +
-        block.bulleted_list_item.rich_text.map((t) => t.plain_text).join("")
-      );
-    }
-
-    if (block.numbered_list_item && block.numbered_list_item.rich_text) {
-      return (
-        "1. " +
-        block.numbered_list_item.rich_text.map((t) => t.plain_text).join("")
-      );
-    }
-
-    if (block.to_do && block.to_do.rich_text) {
-      return "[ ] " + block.to_do.rich_text.map((t) => t.plain_text).join("");
-    }
-
-    if (block.toggle && block.toggle.rich_text) {
-      return "▸ " + block.toggle.rich_text.map((t) => t.plain_text).join("");
-    }
-
     return "";
   }
 
